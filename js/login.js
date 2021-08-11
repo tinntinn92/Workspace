75b6f7ab96b8de07ffe79f730aafc8ebc962ad73
tinntinn92 <tinchooo92@gmail.com>
2021-08-11 09:37:11 -0300

    Agrego login

--- js/login.js
+++ js/login.js
@@ -1,25 +1,29 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
 
 });
 
-const datos = () => {
+
+const login = () => {
 
     let usuario = document.getElementById('user');
     let pass = document.getElementById('pass');
-    let datos = {};
+
 
     if (usuario.value.trim() == '' || pass.value.trim() == '') {
         alert('Falta completar algun campo');
     } else {
+        let datos = {};
 
         datos.usuario = usuario.value;
         datos.pass = pass.value;
         datos.estado = 'conectado';
 
-        location.href = 'index.html';
+        conectado = true
+
+        location.href = '../Workspace/index.html';
 
         sessionStorage.setItem('datos', JSON.stringify(datos));
 
