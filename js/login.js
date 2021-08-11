f52efedb7c52e7129e5d0bc443b97ab4f76cf207
tinntinn92 <tinchooo92@gmail.com>
2021-08-11 08:34:36 -0300

    Agrego login en html y js

--- js/login.js
+++ js/login.js
@@ -4,3 +4,25 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+
+const datos = () => {
+
+    let usuario = document.getElementById('user');
+    let pass = document.getElementById('pass');
+    let datos = {};
+
+    if (usuario.value.trim() == '' || pass.value.trim() == '') {
+        alert('Falta completar algun campo');
+    } else {
+
+        datos.usuario = usuario.value;
+        datos.pass = pass.value;
+        datos.estado = 'conectado';
+
+        location.href = 'index.html';
+
+        sessionStorage.setItem('datos', JSON.stringify(datos));
+
+
+    }
+}
