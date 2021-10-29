e8fd81abfe24a848877d24003a22006cc44746ec
Tincho <tinchooo92@gmail.com>
2021-10-29 13:41:57 -0300

    actualziado

--- js/my-profile.js
+++ js/my-profile.js
@@ -5,13 +5,19 @@ let datos= JSON.parse(localStorage.getItem('datos'));
 
 const showProfile=()=>{
     document.getElementById('perfil').innerHTML=`
-    <img src="${datos.img}" alt="">
-    <ul>
-        <li> Nombre: ${datos.usuario}</li>
-        <li> Edad: ${datos.edad}</li>
-        <li> E-mail: ${datos.email}</li>
-        <li> Telefono: ${datos.telefono}</li>
-    </ul>
+    <div class='row'>
+        <div class= 'col-sm'>
+            <img src="${datos.img}" alt="">
+        </div>
+        <div class='col-sm'>
+            <ul>
+                <li> Nombre: ${datos.usuario}</li>
+                <li> Edad: ${datos.edad}</li>
+                <li> E-mail: ${datos.email}</li>
+                <li> Telefono: ${datos.telefono}</li>
+            </ul>
+        </div>
+    </div>
     `
     
     
@@ -19,6 +25,8 @@ const showProfile=()=>{
 
 const enviarDatos= ()=>{
 
+    
+    
     let nombre= document.getElementById('nombre').value;
     let apellido= document.getElementById('apellido').value;
     let nombreCompleto=nombre+' '+apellido
@@ -29,7 +37,19 @@ const enviarDatos= ()=>{
     datos.telefono=document.getElementById('telefono').value;
 
     localStorage.setItem('datos',JSON.stringify(datos));
-    showProfile();
+    
+    if(nombreCompleto.trim()=='' || datos.edad=='' || datos.email.trim()=='' || datos.telefono.trim()=='' ){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Falta completar algun campo',
+
+        })
+    }
+    else{
+        showProfile();
+    
+    }
     
 
 
